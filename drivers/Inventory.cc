27c05d0c303294106da7990488014300d516bcdc
Diego Garcia Padilla <gpdiego370@outlook.com>
2023-01-03 13:18:03 -0600

    Update Inventory class

--- drivers/Inventory.cc
+++ drivers/Inventory.cc
@@ -4,13 +4,18 @@
  * @brief This file contains the implementation of the Inventory class
  * @version 0.1
  * @date 2023-01-03
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #ifndef INVENTORY_CC // If the file is not defined
 #define INVENTORY_CC // Define the file
+
+#include <iostream> // Include input and output library
+#include <string>   // Include string library
+#include <sstream>  // Include string stream library
+#include <vector>   // Include vector library
 
 #include "../headers/Inventory.h" // Include the header file
 
@@ -18,7 +23,7 @@
 
 /**
  * @brief Construct a new Inventory:: Inventory object
- * 
+ *
  */
 
 Inventory::Inventory()
@@ -29,8 +34,8 @@ Inventory::Inventory()
 
 /**
  * @brief Construct a new Inventory:: Inventory object
- * 
- * @param capacity 
+ *
+ * @param capacity
  */
 
 Inventory::Inventory(int capacity)
@@ -43,8 +48,8 @@ Inventory::Inventory(int capacity)
 
 /**
  * @brief Get the Max Weight object
- * 
- * @return int 
+ *
+ * @return int
  */
 
 int Inventory::getMaxWeight()
@@ -54,8 +59,8 @@ int Inventory::getMaxWeight()
 
 /**
  * @brief Get the Current Weight object
- * 
- * @return int 
+ *
+ * @return int
  */
 
 int Inventory::getCurrentWeight()
@@ -67,8 +72,8 @@ int Inventory::getCurrentWeight()
 
 /**
  * @brief Add a weapon to the inventory
- * 
- * @param weapon 
+ *
+ * @param weapon
  */
 
 void Inventory::addWeapon(Weapon weapon)
@@ -79,8 +84,8 @@ void Inventory::addWeapon(Weapon weapon)
 
 /**
  * @brief Add an armor to the inventory
- * 
- * @param armor 
+ *
+ * @param armor
  */
 
 void Inventory::addArmor(Armor armor)
@@ -91,8 +96,8 @@ void Inventory::addArmor(Armor armor)
 
 /**
  * @brief Add a consumable to the inventory
- * 
- * @param consumable 
+ *
+ * @param consumable
  */
 
 void Inventory::addConsumable(Consumable consumable)
@@ -103,8 +108,8 @@ void Inventory::addConsumable(Consumable consumable)
 
 /**
  * @brief Add a quest item to the inventory
- * 
- * @param questItem 
+ *
+ * @param questItem
  */
 
 void Inventory::addQuestItem(QuestItem questItem)
@@ -115,8 +120,8 @@ void Inventory::addQuestItem(QuestItem questItem)
 
 /**
  * @brief Remove a weapon from the inventory
- * 
- * @param weapon 
+ *
+ * @param weapon
  */
 
 void Inventory::removeWeapon(Weapon weapon)
@@ -133,8 +138,8 @@ void Inventory::removeWeapon(Weapon weapon)
 
 /**
  * @brief Remove an armor from the inventory
- * 
- * @param armor 
+ *
+ * @param armor
  */
 
 void Inventory::removeArmor(Armor armor)
@@ -151,8 +156,8 @@ void Inventory::removeArmor(Armor armor)
 
 /**
  * @brief Remove a consumable from the inventory
- * 
- * @param consumable 
+ *
+ * @param consumable
  */
 
 void Inventory::removeConsumable(Consumable consumable)
@@ -169,8 +174,8 @@ void Inventory::removeConsumable(Consumable consumable)
 
 /**
  * @brief Remove a quest item from the inventory
- * 
- * @param questItem 
+ *
+ * @param questItem
  */
 
 void Inventory::removeQuestItem(QuestItem questItem)
@@ -187,17 +192,24 @@ void Inventory::removeQuestItem(QuestItem questItem)
 
 /**
  * @brief Show the inventory
- * 
+ *
  */
 
 void Inventory::showInventory()
 {
     int itemNum = 1;
 
+    std::cout << "INVENTORY" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Current Weight: " << currentWeight << std::endl;
+    std::cout << "Max Weight: " << maxWeight << std::endl;
+    std::cout << std::endl;
+
     std::cout << "Weapons: " << std::endl;
     for (int i = 0; i < weapons.size(); i++)
     {
-       std:: cout << itemNum << ". " << weapons[i].getName() << std::endl;
+        std::cout << itemNum << ". " << weapons[i].getName() << std::endl;
         itemNum++;
     }
 
--- headers/Inventory.h
+++ headers/Inventory.h
@@ -4,18 +4,18 @@
  * @brief This class represents the inventory of the player
  * @version 0.1
  * @date 2023-01-02
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #ifndef INVENTORY_H // If the preprocessor INVENTORY_H is not defined
 #define INVENTORY_H // Define the preprocessor INVENTORY_H
 
-#include "../drivers/Weapon.cc" // Include the Weapon class
-#include "../drivers/Armor.cc" // Include the Armor class
+#include "../drivers/Weapon.cc"     // Include the Weapon class
+#include "../drivers/Armor.cc"      // Include the Armor class
 #include "../drivers/Consumable.cc" // Include the Consumable class
-#include "../drivers/QuestItem.cc" // Include the QuestItem class
+#include "../drivers/QuestItem.cc"  // Include the QuestItem class
 
 class Inventory
 {
@@ -46,10 +46,12 @@ public:
     void addArmor(Armor armor);
     void addConsumable(Consumable consumable);
     void addQuestItem(QuestItem questItem);
+
     void removeWeapon(Weapon weapon);
     void removeArmor(Armor armor);
     void removeConsumable(Consumable consumable);
     void removeQuestItem(QuestItem questItem);
+    
     void showInventory();
 };
 
