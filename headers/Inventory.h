8817bbfbe79a3838acd99fe9bde4a30a75773354
Diego Garcia Padilla <gpdiego370@outlook.com>
2023-01-02 20:15:40 -0600

    Update Inventory.h

--- headers/Inventory.h
+++ headers/Inventory.h
@@ -26,3 +26,34 @@ private:
     std::vector<QuestItem> questItems;
 
 public:
+    // Constructors
+    Inventory();
+    Inventory(int maxWeight, int maxItems);
+
+    // Getters
+    int getMaxWeight();
+    int getMaxItems();
+    int getCurrentWeight();
+    int getCurrentItems();
+    std::vector<Weapon> getWeapons();
+    std::vector<Armor> getArmors();
+    std::vector<Consumable> getConsumables();
+    std::vector<QuestItem> getQuestItems();
+
+    // Setters
+    void setMaxWeight(int maxWeight);
+    void setMaxItems(int maxItems);
+    void setCurrentWeight(int currentWeight);
+    void setCurrentItems(int currentItems);
+    void setWeapons(std::vector<Weapon> weapons);
+    void setArmors(std::vector<Armor> armors);
+    void setConsumables(std::vector<Consumable> consumables);
+    void setQuestItems(std::vector<QuestItem> questItems);
+
+    // Methods
+    void addItem(Item item);
+    void removeItem(Item item);
+    void showInventory();
+};
+
+#endif // End of the preprocessor INVENTORY_H
